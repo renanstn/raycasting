1ec0e0c9c30a554767f4131900982a7d3317afcf
Renan Santana <renan_ota@hotmail.com>
2021-07-15 19:06:26 -0300

    Aplica fórmulas de construção do mundo 3D no raycasting

--- src/raycasting.py
+++ src/raycasting.py
@@ -13,13 +13,32 @@ def raycasting(screen, player_position: tuple, player_angle: int):
         for depth in range(settings.MAX_DEPTH):
             x = xo + depth * cos_a
             y = yo + depth * sin_a
-            pygame.draw.line(
-                screen,
-                settings.DARKGRAY,
-                player_position,
-                (x, y),
-                2
-            )
+            # pygame.draw.line(
+            #     screen,
+            #     settings.DARKGRAY,
+            #     player_position,
+            #     (x, y),
+            #     2
+            # )
             if (x // settings.TILE * settings.TILE, y // settings.TILE * settings.TILE) in world_map:
+                # Corrige efeito de "olho de peixe"
+                depth *= math.cos(player_angle - cur_angle)
+                # Calcula a altura de cada linha de projeção
+                projection_height = settings.PROJECTION_COEFFICIENT / depth
+                # Calcula a cor da parede de acordo com a "distância" da mesma
+                rgb_color = 255 / (1 + depth * depth * 0.0001)
+                # color = (rgb_color, rgb_color, rgb_color)
+                color = (rgb_color // 2, rgb_color, rgb_color // 3)
+                # Desenha o mundo 3D, toda a magia acontece bem aqui:
+                pygame.draw.rect(
+                    screen,
+                    color,
+                    (
+                        ray * settings.SCALE,
+                        settings.HALF_HEIGHT - projection_height // 2,
+                        settings.SCALE,
+                        projection_height
+                    )
+                )
                 break
         cur_angle += settings.DELTA_ANGLE

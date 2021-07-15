1a8b0e2e8a11ad781e57f65b308f1be3572f77bb
Renan Santana <renan_ota@hotmail.com>
2021-07-15 13:04:32 -0300

    Define settings

---
+++ src/settings.py
@@ -0,0 +1,20 @@
+# Game settings
+WIDTH = 1200
+HEIGHT = 800
+HALF_WIDTH = WIDTH / 2
+HALF_HEIGHT = HEIGHT / 2
+FPS = 60
+
+# Player settings
+player_position = (HALF_WIDTH, HALF_HEIGHT)
+player_angle = 0
+player_speed = 2
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (220, 0, 0)
+GREEN = (0, 220, 0)
+BLUE = (0, 0, 255)
+DARKGRAY = (40, 40, 40)
+PURPLE = (120, 0, 120)
